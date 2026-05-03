2962721b3f4451bb36f610a57890608511e69c8b
Joao Alexandre <joaossouto55@gmail.com>
2026-05-03 14:28:40 -0300

    atualizaçãod eo de segurança adicionado

---
+++ src/projeto/auth/AuthService.java
@@ -0,0 +1,32 @@
+package projeto.auth;
+
+import projeto.entidades.Usuario;
+import projeto.exception.NegocioException;
+import projeto.services.UsuarioService;
+import projeto.utils.SenhaUtil;
+
+import java.util.Optional;
+
+public class AuthService {
+
+    private UsuarioService usuarioService;
+
+    public AuthService(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    public void logar(String email, String senha){
+        Usuario user = usuarioService.listar().stream()
+                .filter(u -> u.getEmail().equals(email))
+                .findFirst()
+                .orElseThrow(() -> new NegocioException("Usuario não encontrado"));
+
+        if (SenhaUtil.comparar(senha, user.getSenha())){
+            Sessao.logar(user);
+        }else {
+            throw new NegocioException("Login inválido");
+        }
+    }
+
+
+}
